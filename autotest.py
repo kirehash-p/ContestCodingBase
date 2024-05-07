4bbb4f6a146982a5c4386ac544ee708360ce9124
kirehash-p <kirehash.d@gmail.com>
2024-05-07 14:01:28 +0900

    Fixed the autotest bug

--- autotest.py
+++ autotest.py
@@ -6,13 +6,13 @@ def run_and_compare(folder_path, run_command):
     """テストケースを実行し、出力を比較"""
     testcases = len([f for f in os.listdir(folder_path) if f.startswith('input')])
     for i in range(testcases):
-        with open(f'{folder_path}/input_{i}.txt', 'r') as f:
+        with open(f'{folder_path}/input_{i+1}.txt', 'r') as f:
             input_data = f.read()
-        with open(f'{folder_path}/output_{i}.txt', 'r') as f:
+        with open(f'{folder_path}/output_{i+1}.txt', 'r') as f:
             expected_output = f.read()
         process = subprocess.run(run_command, input=input_data.encode(), stdout=subprocess.PIPE)
         if process.stdout.decode() != expected_output:
-            print(f'Failed on test case {i}')
+            print(f'Failed on test case {i+1}')
             print('Input:')
             print(input_data)
             print('Expected output:')
@@ -20,13 +20,13 @@ def run_and_compare(folder_path, run_command):
             print('Your output:')
             print(process.stdout.decode())
         else:
-            print(f'Passed on test case {i}')
+            print(f'Passed on test case {i+1}')
 
 def build_and_run(file_or_folder_path):
     """フォルダ内のファイルをコンパイルし、テストケースを実行"""
     # 与えられたパスからフォルダ名とファイル名を取得
     folder_path = os.path.dirname(file_or_folder_path)
-    file_name = os.path.basename(file_or_folder_path)
+    file_name = os.path.splitext(os.path.basename(file_or_folder_path))[0]
     # ファイル名が指定されていない場合、フォルダ名をファイル名とする
     if not file_name:
         file_name = folder_path.split('/')[-1]
@@ -41,18 +41,23 @@ def build_and_run(file_or_folder_path):
             if file.endswith('.cpp') or file.endswith('.cc'):
                 main_file = a('main')
                 # コンパイル
+                print(f'Compiling {file} ...', end='')
                 subprocess.run(['g++', '-o', main_file, a(file)])
+                print('Done')
                 run_command = [main_file]
             elif file.endswith('.py'):
                 run_command = ['python', a(file)]
             elif file.endswith('.rs'):
                 main_file = a('main')
                 # コンパイル
+                print(f'Compiling {file} ...', end='')
                 subprocess.run(['rustc', '-o', main_file, a(file)])
+                print('Done')
                 run_command = [main_file]
             else:
                 continue
             # テストケースを実行
+            print(f'Running test cases ...', end='')
             run_and_compare(folder_path, run_command)
 
 if __name__ == "__main__":
